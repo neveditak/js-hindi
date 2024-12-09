c5075a3e46436376eeef3753a8d66f5a3264e209
neveditak <engr.nk2015@outlook.com>
2024-12-09 11:18:46 +0000

    commit3

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,11 +2,14 @@ const accountId = 22334455
 let accountEmail = "nevedita@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-let stateName;
+let stateName;      //undefined 
 
 // accountId = 2
 accountEmail = "nk@gmail.com"
 
+/*
+Prefer not use var because of issue in block scope and functional scope
+*/
 console.log(accountId);
 console.log(accountEmail);
 console.table([accountId, accountEmail, accountPassword, accountCity, stateName])
