5b878573399583eed4089ef0dbff13bf870a6c55
neveditak <engr.nk2015@outlook.com>
2024-12-09 10:54:43 +0000

    commit2

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,7 +2,11 @@ const accountId = 22334455
 let accountEmail = "nevedita@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let stateName;
 
-accountId = 2
+// accountId = 2
+accountEmail = "nk@gmail.com"
 
 console.log(accountId);
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity, stateName])
