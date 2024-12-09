36061eaa325b55d61cb3707abb1819b49ffa0f6f
neveditak <engr.nk2015@outlook.com>
2024-12-09 12:27:12 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 +3) //we are using nodejs, not browser
+//alert("Hello") //we are using nodejs, not browser
+
+//MDN
+//ecma Script
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value 
+//undefined
+//symbol => unique
+
+//object 
+
+console.log(typeof "Hitesh");   //string
+
+let age = 18                    //number
+console.log(typeof age);
+
+console.log(typeof null)        //object
+
+console.log(typeof undefined)   //undefined  a type
+
